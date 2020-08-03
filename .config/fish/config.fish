5cb07ebbdf448858b20dfa49d7e8de6c90d70fbf
Moisés Carvalho <moisescarv27@hotmail.com>
2020-08-02 23:24:35 -0300

    Initial commit

---
+++ .config/fish/config.fish
@@ -0,0 +1,21 @@
+#!/usr/bin/env fish
+
+# Environment variables
+set -x EDITOR vim
+set -x VISUAL vim
+set fish_greeting
+
+# Alias
+alias b  bat
+alias c  clear
+alias g  git
+alias ls lsd
+alias ll 'lsd -l --blocks permission,size,date,name --date relative'
+alias m  man
+alias rs cargo
+alias s  ssh
+alias t  tmux
+alias v  vim
+alias vf vifm
+alias y  yarn
+
