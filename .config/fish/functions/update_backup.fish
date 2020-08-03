e5bb3b53e7544806518479267f17b83985af710e
Moisés Carvalho <moisescarv27@hotmail.com>
2020-08-03 08:56:27 -0300

    Swap characters in Termux keyboard

--- .config/fish/functions/update_backup.fish
+++ .config/fish/functions/update_backup.fish
@@ -2,11 +2,11 @@
 
 function update_backup
   B status -s
-  set files (B status -s | awk '{ print $2  }')
+  set files (B status -s | awk '{ print $2 }')
 
   if test -n "$files"
     B add -u
-    read -p 'Commit message: ' msg
+    read -P 'Commit message> ' msg
     B commit -m $msg
     B push
   end
