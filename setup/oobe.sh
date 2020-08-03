bdd5a083eb4b52782308ffbbbd906822a9550d85
Moisés Carvalho <moisescarv27@hotmail.com>
2020-08-03 14:08:44 -0300

    Add commentary and Oh-My-Fish!

--- setup/oobe.sh
+++ setup/oobe.sh
@@ -1,16 +1,22 @@
 #!/usr/bin/env bash
 
+# Updates
 pkg update
 pkg upgrade
 
+# User packages
 pkg install $(cat apt-packages.txt)
 
+# Fish
 ./fish-variables.fish
+fish <(curl -sL https://get.oh-my.fish) --noninteractive
 
+# Vim plugins
 for REPO in $(awk '{print "https://github.com/"$1" "$2}' repos.txt)
 do
   $REPO
 done
 
+# Rust
 cargo install lsd
 
