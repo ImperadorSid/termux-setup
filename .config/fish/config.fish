eed28553ce11c52cee4733ea9f731f249eb879c7
Moisés Carvalho <moisescarv27@hotmail.com>
2020-08-03 09:47:19 -0300

    Ignore multiple folders

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,8 +10,6 @@ alias b  bat
 alias B  'g --git-dir ~/backup --work-tree ~'
 alias c  clear
 alias g  git
-alias ls lsd
-alias ll 'lsd -l --blocks permission,size,date,name --date relative'
 alias m  man
 alias rs cargo
 alias s  ssh
@@ -19,6 +17,11 @@ alias t  tmux
 alias v  vim
 alias vf vifm
 alias y  yarn
+
+# Default options
+alias fd 'fd -IHL --ignore-file ~/.config/git/ignore'
+alias ll 'lsd -l --blocks permission,size,date,name --date relative'
+alias ls lsd
 
 # Additional settings
 functions -e fish_right_prompt
